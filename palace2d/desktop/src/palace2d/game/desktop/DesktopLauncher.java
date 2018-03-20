dc1bd598daece92f39ab3deaddd150b596d21db5
Michał Sarzyński <ms371678@students.mimuw.edu.pl>
2018-03-20 09:43:39 +0100

    libGDX + Box2d

---
+++ palace2d/desktop/src/palace2d/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package palace2d.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import palace2d.game.Palace2D;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Palace2D(), config);
+	}
+}
