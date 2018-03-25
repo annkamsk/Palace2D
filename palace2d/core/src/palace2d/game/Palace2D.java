5b4567582caf255a8669452d35f118efe16ceb17
Anna Kramarska <ak385833@students.mimuw.edu.pl>
2018-03-25 21:59:27 +0200

    Board with first level block

--- palace2d/core/src/palace2d/game/Palace2D.java
+++ palace2d/core/src/palace2d/game/Palace2D.java
@@ -2,32 +2,66 @@ package palace2d.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Palace2D extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Texture block1, background;
+	private State state;
+
+	private enum State {
+		win,
+		lose,
+		play
+	}
+
+
+	private void newGame() {
+		state = State.play;
 	}
 
 	@Override
+	public void create () {
+		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch = new SpriteBatch();
+		block1 = new Texture(Gdx.files.internal("block1.png"));
+		background = new Texture(Gdx.files.internal("background.png"));
+		newGame();
+	}
+
+	public void resize(int width, int height) {
+		camera.setToOrtho(false, 800, 600);
+	}
+
+
+	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0, 0);
+		if (state == State.play) {
+			batch.draw(block1,111,10);
+		} else if (state == State.lose) {
+			// batch.draw(lose, 0,0);
+		} else if (state == State.win) {
+			// batch.draw(win,0,0);
+		}
+
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		block1.dispose();
+		background.dispose();
+
 	}
 }
--- palace2d/desktop/src/palace2d/game/desktop/DesktopLauncher.java
+++ palace2d/desktop/src/palace2d/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import palace2d.game.Palace2D;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Palace2D";
+		config.width = 800;
+		config.height = 600;
 		new LwjglApplication(new Palace2D(), config);
 	}
 }
