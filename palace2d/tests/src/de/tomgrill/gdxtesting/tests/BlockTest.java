b234ada799b2230a977afe175543c204215843dc
Michał Sarzyński <ms371678@students.mimuw.edu.pl>
2018-04-23 11:01:52 +0200

    Testing module and Block class tests added

---
+++ palace2d/tests/src/de/tomgrill/gdxtesting/tests/BlockTest.java
@@ -0,0 +1,58 @@
+package de.tomgrill.gdxtesting.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import com.badlogic.gdx.graphics.Texture;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.badlogic.gdx.Gdx;
+
+import de.tomgrill.gdxtesting.GdxTestRunner;
+import palace2d.game.Block;
+
+@RunWith(GdxTestRunner.class)
+public class BlockTest {
+
+    private Block block;
+    private Texture blockTexture;
+
+    @Before
+    public void setUp() {
+        blockTexture = new Texture(Gdx.files.internal("../core/assets/block0.png"));
+        block = new Block(blockTexture, 0);
+    }
+
+    @Test
+    public void blockConstructorTest() {
+        assertNotNull(block);
+    }
+
+    @Test
+    public void spriteTextureTest() {
+        assertEquals(block.getSprite().getTexture(), blockTexture);
+    }
+
+    @Test
+    public void spritePosTest() {
+        block.spritePos(10f, 10f);
+        assertEquals(10f, block.getSprite().getX(), 0f);
+        assertEquals(10f, block.getSprite().getY(), 0f);
+
+        block.spritePos(0f, 0f);
+        assertEquals(0f, block.getSprite().getY(), 0f);
+        assertEquals(0f, block.getSprite().getY(), 0f);
+    }
+
+    @Test
+    public void spriteTrimTest() {
+        block.trim(10);
+        assertEquals(10f, block.getSprite().getWidth(), 0f);
+
+        block.trim(100);
+        assertEquals(100f, block.getSprite().getWidth(), 0f);
+    }
+}
+
