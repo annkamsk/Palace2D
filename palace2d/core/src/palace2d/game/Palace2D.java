0e253f39262032066281e7111935892dc1a22f60
Michał Sarzyński <ms371678@students.mimuw.edu.pl>
2018-05-05 20:41:39 +0200

    Camera movement added

--- palace2d/core/src/palace2d/game/Palace2D.java
+++ palace2d/core/src/palace2d/game/Palace2D.java
@@ -10,35 +10,17 @@ import palace2d.game.Screens.MainMenuScreen;
 
 
 public class Palace2D extends Game {
-    public static final String TITLE = "Palace2D";
-    public static final int V_WIDTH = 800;
-    public static final int V_HEIGHT = 600;
-
 	public static final String TITLE = "Palace2D";
 	public static final int V_WIDTH = 800;
 	public static final int V_HEIGHT = 600;
 
-	public int score;
-	public String scoreName;
-
 	public OrthographicCamera camera;
-	public SpriteBatch batch;
-
-	public BitmapFont font;
 
 	@Override
 	public void create () {
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, V_WIDTH, V_HEIGHT);
         this.setScreen(new MainMenuScreen(this));
-
-        batch = new SpriteBatch();
-
-        font = new BitmapFont();
-        font.setColor(Color.WHITE);
-
-        score = 0;
-        scoreName = "score: 0";
 	}
 
 	public void resize(int width, int height) {}
@@ -50,6 +32,5 @@ public class Palace2D extends Game {
 
 	@Override
 	public void dispose () {
-		batch.dispose();
 	}
 }
