249f85dbebfa67786d6f489948e853db357c3f5f
Anna Kramarska <ak385833@students.mimuw.edu.pl>
2018-05-21 14:27:41 +0200

    Import pakietu Input

--- palace2d/core/src/palace2d/game/Screens/EndGameScreen.java
+++ palace2d/core/src/palace2d/game/Screens/EndGameScreen.java
@@ -1,6 +1,7 @@
 package palace2d.game.Screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
